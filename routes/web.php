202f6cdefeff1b043c7c73dd93fada2cd4576fa1
Igor <shedowfox1987@gmail.com>
2024-03-13 13:50:05 +0500

    7 практика

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\CatController;
+use App\Http\Controllers\Admin\TagController;
+
 
 
 /*
@@ -24,7 +25,8 @@ Route::get('/', function () {
 Route::prefix('admin')->group(function () {
     Route::get('/', [MainController::class, 'index']) ->name('admin.index');
     Route::resource('/categories', CategoryController::class);
-    Route::get('/cat', [CatController::class, 'index']) ->name('admin.cat');
+    Route::resource('/tags', TagController::class);
+    
 });
 
 // Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
